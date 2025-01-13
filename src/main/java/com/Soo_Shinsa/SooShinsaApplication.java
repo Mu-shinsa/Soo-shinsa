c4cf36668467531a7ccea2cc850c5a25b8eb9f63
이자드 <kimminkyeong0418@gmail.com>
2025-01-13 15:11:00 +0900

    feat : baseEntity 생성
    생성 일시, 수정 일시 포함 entity

--- src/main/java/com/Soo_Shinsa/SooShinsaApplication.java
+++ src/main/java/com/Soo_Shinsa/SooShinsaApplication.java
@@ -2,7 +2,9 @@ package com.Soo_Shinsa;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableJpaAuditing
 @SpringBootApplication
 public class SooShinsaApplication {
 
