560b24b5898aa77840bcc1bc0407396a108dc2a2
이자드 <kimminkyeong0418@gmail.com>
2025-01-14 11:13:24 +0900

    build : 프로젝트 기본 세팅
    - Java 17
    - Spring Boot 3.4.1
    - mysql
    - jpa
    - Spring Security

---
+++ src/main/java/com/Soo_Shinsa/SooShinsaApplication.java
@@ -0,0 +1,13 @@
+package com.Soo_Shinsa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SooShinsaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SooShinsaApplication.class, args);
+	}
+
+}
