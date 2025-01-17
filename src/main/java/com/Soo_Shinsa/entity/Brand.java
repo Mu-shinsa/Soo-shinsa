694c3d23ad056c34ed57a0ee997ecca08d053bd2
kjs0406 <rnjswlstjr19089@gmail.com>
2025-01-17 20:58:57 +0900

    Fix: brand CRUD 작성

---
+++ src/main/java/com/Soo_Shinsa/entity/Brand.java
@@ -0,0 +1,57 @@
+package com.Soo_Shinsa.entity;
+
+import com.Soo_Shinsa.model.BaseTimeEntity;
+import com.Soo_Shinsa.model.User;
+import jakarta.persistence.Entity;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.DynamicInsert;
+import org.hibernate.annotations.DynamicUpdate;
+import org.hibernate.validator.constraints.Length;
+
+@Entity
+@DynamicInsert
+@DynamicUpdate
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Brand  extends BaseTimeEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    @Length(min = 1, max = 50)
+    private String registrationNum;
+
+    @Column(nullable = false)
+    @Length(min = 1, max = 255)
+    private String name;
+
+    @Column(columnDefinition = "TEXT")
+    private String context;
+
+    @Column(nullable = false)
+    @Length(min = 1, max = 15)
+    private String status;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false)
+    private User userId;
+
+    public Brand(String registrationNum, String name, String context, String status, User userId) {
+        this.registrationNum = registrationNum;
+        this.name = name;
+        this.context = context;
+        this.status = status;
+        this.userId = userId;
+    }
+
+    public void refuseBrand(String status, String context) {
+        this.status = status;
+        this.context = context;
+    }
+}
