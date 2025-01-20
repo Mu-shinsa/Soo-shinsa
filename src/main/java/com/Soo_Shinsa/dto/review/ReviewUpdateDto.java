04017e05ead13b8352587308234855e30f3696cf
FANG <107174112+Fangsangik@users.noreply.github.com>
2025-01-20 12:20:33 +0900

    feat : "ReviewUpdateDto" - 리뷰 수정 Dto

---
+++ src/main/java/com/Soo_Shinsa/dto/review/ReviewUpdateDto.java
@@ -0,0 +1,19 @@
+package com.Soo_Shinsa.dto.review;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+public class ReviewUpdateDto {
+
+    private Long id;
+    private Integer rate;
+    private String content;
+
+    public ReviewUpdateDto(Long id, Integer rate, String content) {
+        this.id = id;
+        this.rate = rate;
+        this.content = content;
+    }
+}
