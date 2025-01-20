efd5e76ec39fe86164ffa4e9c2578d824fae9120
FANG <107174112+Fangsangik@users.noreply.github.com>
2025-01-20 22:32:34 +0900

    refactor : "ReviewController 수정" - getAllReviewByProductId : 해당 제품에 대한 Review

--- src/main/java/com/Soo_Shinsa/controller/ReviewController.java
+++ src/main/java/com/Soo_Shinsa/controller/ReviewController.java
@@ -9,6 +9,9 @@ import com.Soo_Shinsa.utils.UserUtils;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
@@ -42,14 +45,6 @@ public class ReviewController {
         return ResponseEntity.ok(review);
     }
 
-    @GetMapping("/product/{productId}")
-    public ResponseEntity<Page<ReviewResponseDto>> getReviewProduct(@PathVariable Long productId,
-                                                                    @RequestParam(defaultValue = "0") int page,
-                                                                    @RequestParam(defaultValue = "10") int size) {
-        Page<ReviewResponseDto> reviews = reviewService.getAllReviewProduct(productId, page, size);
-        return ResponseEntity.ok(reviews);
-    }
-
     @PatchMapping("/{reviewId}")
     public ResponseEntity<ReviewUpdateDto> updateReview(@PathVariable Long reviewId,
                                                         @Valid @RequestBody ReviewUpdateDto updateDto,
@@ -61,9 +56,8 @@ public class ReviewController {
 
     @GetMapping("/product/{productId}")
     public ResponseEntity<Page<ReviewResponseDto>> getAllReviewByProductId(@PathVariable Long productId,
-                                                                          @RequestParam(defaultValue = "0") int page,
-                                                                          @RequestParam(defaultValue = "10") int size) {
-        Page<ReviewResponseDto> reviews = reviewService.getAllReviewProduct(productId, page, size);
+                                                                           @PageableDefault(size = 10, sort = "createdAt", direction = Sort.Direction.DESC) Pageable pageable) {
+        Page<ReviewResponseDto> reviews = reviewService.getAllReviewProduct(productId, pageable);
         return ResponseEntity.ok(reviews);
     }
 
