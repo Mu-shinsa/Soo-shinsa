4c0ba6e8f0cff2d75845e88a6d6f1b72c10b6563
FANG <107174112+Fangsangik@users.noreply.github.com>
2025-01-20 22:33:14 +0900

    refactor : "ReviewRepository 수정" - findAllByProductId : 해당 제품에 대한 Review

--- src/main/java/com/Soo_Shinsa/repository/ReviewRepository.java
+++ src/main/java/com/Soo_Shinsa/repository/ReviewRepository.java
@@ -3,10 +3,6 @@ package com.Soo_Shinsa.repository;
 import com.Soo_Shinsa.entity.Review;
 import org.springframework.data.domain.Page;
 
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ReviewRepository extends JpaRepository<Review, Long> {
-    Page<Review> findByProductId(Long productId, int page, int size);
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
