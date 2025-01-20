e2033ef71dc22ed3103fa65869c829c63158d6ca
FANG <107174112+Fangsangik@users.noreply.github.com>
2025-01-20 13:47:45 +0900

    refactor : "ReviewController" - valid 추가 - @AuthenticationPrincipal로 인증 인가 부분 처리 - @PathVariable 빼먹은 부분 추가

--- src/main/java/com/Soo_Shinsa/controller/ReviewController.java
+++ src/main/java/com/Soo_Shinsa/controller/ReviewController.java
@@ -1,12 +1,16 @@
 package com.Soo_Shinsa.controller;
 
+import com.Soo_Shinsa.auth.UserDetailsImp;
 import com.Soo_Shinsa.dto.ReviewRequestDto;
 import com.Soo_Shinsa.dto.ReviewResponseDto;
 import com.Soo_Shinsa.dto.ReviewUpdateDto;
 import com.Soo_Shinsa.service.ReviewService;
+import com.Soo_Shinsa.utils.UserUtils;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -17,7 +21,10 @@ public class ReviewController {
     private final ReviewService reviewService;
 
     @PostMapping("/order-item/{orderItemId}")
-    public ResponseEntity<ReviewResponseDto> createReview(@PathVariable Long orderItemId, @RequestBody ReviewRequestDto requestDto) {
+    public ResponseEntity<ReviewResponseDto> createReview(@PathVariable Long orderItemId,
+                                                          @Valid @RequestBody ReviewRequestDto requestDto,
+                                                          @AuthenticationPrincipal UserDetailsImp userDetails) {
+        UserUtils.getUser(userDetails);
         ReviewResponseDto review = reviewService.createReview(orderItemId, requestDto);
         return ResponseEntity.ok(review);
     }
@@ -29,14 +36,20 @@ public class ReviewController {
     }
 
     @PatchMapping("/{reviewId}")
-    public ResponseEntity<ReviewUpdateDto> updateReview(Long reviewId, @RequestBody ReviewUpdateDto updateDto) {
+    public ResponseEntity<ReviewUpdateDto> updateReview(@PathVariable Long reviewId,
+                                                        @Valid @RequestBody ReviewUpdateDto updateDto,
+                                                        @AuthenticationPrincipal UserDetailsImp userDetails) {
+        UserUtils.getUser(userDetails);
         ReviewUpdateDto review = reviewService.updateReview(reviewId, updateDto);
         return ResponseEntity.ok(review);
     }
 
     @DeleteMapping("/{reviewId}")
-    public ResponseEntity<Void> deleteReview(Long reviewId) {
+    public ResponseEntity<Void> deleteReview(@PathVariable Long reviewId,
+                                             @AuthenticationPrincipal UserDetailsImp userDetails) {
+        UserUtils.getUser(userDetails);
         reviewService.delete(reviewId);
         return new ResponseEntity<>(HttpStatus.OK);
     }
 }
+
