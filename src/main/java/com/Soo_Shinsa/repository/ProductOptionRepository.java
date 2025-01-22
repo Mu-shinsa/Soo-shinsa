fa8ee119b1ed3fb3021c7ce69289b67c4d87e045
kjs0406 <rnjswlstjr19089@gmail.com>
2025-01-22 20:22:54 +0900

    fix : product,option repository default 추가

--- src/main/java/com/Soo_Shinsa/repository/ProductOptionRepository.java
+++ src/main/java/com/Soo_Shinsa/repository/ProductOptionRepository.java
@@ -3,6 +3,8 @@ package com.Soo_Shinsa.repository;
 import com.Soo_Shinsa.model.Product;
 import com.Soo_Shinsa.model.ProductOption;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
 import java.util.Optional;
@@ -11,4 +13,10 @@ public interface ProductOptionRepository extends JpaRepository<ProductOption, Lo
     Optional<ProductOption> findById(Long id);
 
     List<ProductOption> findAllByProductId(Product productId);
+
+    default ProductOption findByIdOrElseThrow(Long optionId) {
+        return findById(optionId).orElseThrow(
+                () -> new ResponseStatusException(HttpStatus.NOT_FOUND, "해당 옵션을 찾을 수 없습니다.")
+        );
+    }
 }
--- src/main/java/com/Soo_Shinsa/repository/ProductRepository.java
+++ src/main/java/com/Soo_Shinsa/repository/ProductRepository.java
@@ -3,6 +3,8 @@ package com.Soo_Shinsa.repository;
 import com.Soo_Shinsa.model.Brand;
 import com.Soo_Shinsa.model.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
 import java.util.Optional;
@@ -11,4 +13,10 @@ public interface ProductRepository  extends JpaRepository<Product, Long> {
     Optional<Product> findById(Long id);
 
     List<Product> findAllByBrandId(Brand brandId);
+
+    default Product findByIdOrElseThrow(Long productId) {
+        return findById(productId).orElseThrow(
+                () -> new ResponseStatusException(HttpStatus.NOT_FOUND, "해당 상품을 찾을 수 없습니다.")
+        );
+    }
 }
