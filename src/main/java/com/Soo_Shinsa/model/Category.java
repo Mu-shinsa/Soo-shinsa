9b95e5fd73d436c1f6cd808b9c8eb76138d02ea5
kjs0406 <rnjswlstjr19089@gmail.com>
2025-01-23 20:16:24 +0900

    create : category 초기 entity 생성

--- src/main/java/com/Soo_Shinsa/model/Category.java
+++ src/main/java/com/Soo_Shinsa/model/Category.java
@@ -4,6 +4,9 @@ import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Entity
 @Getter
 @NoArgsConstructor
@@ -13,8 +16,21 @@ public class Category extends BaseTimeEntity {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    private Long parentId;
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "parent_id")
+    private Category parent;
+
+    @OneToMany(mappedBy = "parent")
+    private List<Category> children = new ArrayList<>();
 
     @Column(nullable = false)
     private String name;
+
+    public Category(Category parent, String name) {
+        this.parent = parent;
+        this.name = name;
+    }
+    public static Category rootParent() {
+        return new Category();
+    }
 }
