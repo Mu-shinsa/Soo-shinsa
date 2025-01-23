57a36049ca90a6347c45687b777d58693076537f
FANG <107174112+Fangsangik@users.noreply.github.com>
2025-01-23 12:09:05 +0900

    feat : "ReviewDto" - imageUrl 필드 값 추가

--- src/main/java/com/Soo_Shinsa/dto/review/ReviewResponseDto.java
+++ src/main/java/com/Soo_Shinsa/dto/review/ReviewResponseDto.java
@@ -11,18 +11,21 @@ public class ReviewResponseDto {
 
     private Long id;
     private Long userId;
+    private String imageUrl;
     private Long orderItemId;
     private Integer rate;
     private String content;
 
     @Builder
-    public ReviewResponseDto(Long id, Long userId, Long orderItemId, Integer rate, String content) {
+    public ReviewResponseDto(Long id, Long userId, String imageUrl, Long orderItemId, Integer rate, String content) {
         this.id = id;
         this.userId = userId;
+        this.imageUrl = imageUrl;
         this.orderItemId = orderItemId;
         this.rate = rate;
         this.content = content;
     }
+
 
     public static ReviewResponseDto toDto(Review review) {
         return ReviewResponseDto.builder()
@@ -31,6 +34,7 @@ public class ReviewResponseDto {
                 .orderItemId(review.getOrderItem().getId())
                 .rate(review.getRate())
                 .content(review.getContent())
+                .imageUrl(review.getImageUrl())
                 .build();
     }
 }
