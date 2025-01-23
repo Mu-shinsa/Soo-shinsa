4798e825ac06c0b03580b74b62bac734b0b745c5
FANG <107174112+Fangsangik@users.noreply.github.com>
2025-01-23 12:09:50 +0900

    feat : "ReviewService" - Review 생성시 image 추가 가능, - Review 수정시 image 수정 가능

--- src/main/java/com/Soo_Shinsa/service/ReviewService.java
+++ src/main/java/com/Soo_Shinsa/service/ReviewService.java
@@ -6,13 +6,14 @@ import com.Soo_Shinsa.dto.review.ReviewUpdateDto;
 import com.Soo_Shinsa.model.User;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.web.multipart.MultipartFile;
 
 public interface ReviewService {
-    ReviewResponseDto createReview (Long orderItemId, ReviewRequestDto requestDto, User user);
+    ReviewResponseDto createReview(Long orderItemId, ReviewRequestDto requestDto, User user, MultipartFile imageFile);
 
     ReviewResponseDto getReview(Long reviewId);
 
-    ReviewUpdateDto updateReview(Long reviewId, ReviewUpdateDto updateDto, User user);
+    ReviewUpdateDto updateReview(Long reviewId, ReviewUpdateDto updateDto, User user, MultipartFile imageFile);
 
     void delete(Long reviewId, User user);
 
