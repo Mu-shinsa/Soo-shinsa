c767ac77489edaec824138c84ecc20ba945fa1f9
FANG <107174112+Fangsangik@users.noreply.github.com>
2025-01-24 12:31:55 +0900

    feat : "ProductOptionRepository" - deleteAllByProductId : 상품 삭제시 option도 같이 삭제 - findProductsByOptionalSizeAndColor : 사이즈와 색상 조회 - findAllByProductId : 모든 상품 조회

--- src/main/java/com/Soo_Shinsa/repository/ProductOptionRepository.java
+++ src/main/java/com/Soo_Shinsa/repository/ProductOptionRepository.java
@@ -1,14 +1,21 @@
 package com.Soo_Shinsa.repository;
 
-import com.Soo_Shinsa.model.Product;
 import com.Soo_Shinsa.model.ProductOption;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
-import java.util.Optional;
 
 public interface ProductOptionRepository extends JpaRepository<ProductOption, Long> {
-    Optional<ProductOption> findById(Long id);
+    @Query("SELECT po FROM ProductOption po WHERE " +
+            "(:size IS NULL OR po.size = :size) AND " +
+            "(:color IS NULL OR po.color = :color)")
+    Page<ProductOption> findProductsByOptionalSizeAndColor(@Param("size") String size, @Param("color") String color, Pageable pageable);
 
-    List<ProductOption> findAllByProductId(Product productId);
+    List<ProductOption> findAllByProductId(Long productId);
+
+    void deleteAllByProductId(Long productId);
 }
