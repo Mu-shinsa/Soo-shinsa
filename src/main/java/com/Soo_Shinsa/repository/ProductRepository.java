b2f2b70c3b08808774dcd6a2d45d597e8e146ead
FANG <107174112+Fangsangik@users.noreply.github.com>
2025-01-24 12:36:04 +0900

    refactor : "ProductRepository" - Paging 처리 - findAllProductByBrandId : 해당 brand에 모든 상품 조회

--- src/main/java/com/Soo_Shinsa/repository/ProductRepository.java
+++ src/main/java/com/Soo_Shinsa/repository/ProductRepository.java
@@ -1,14 +1,16 @@
 package com.Soo_Shinsa.repository;
 
-import com.Soo_Shinsa.model.Brand;
 import com.Soo_Shinsa.model.Product;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
-import java.util.List;
-import java.util.Optional;
+public interface ProductRepository extends JpaRepository<Product, Long> {
 
-public interface ProductRepository  extends JpaRepository<Product, Long> {
-    Optional<Product> findById(Long id);
+    @Query("SELECT p FROM Product p WHERE p.brand.id = :brandId")
+    Page<Product> findAllProductByBrandId(@Param("brandId") Long brandId, Pageable pageable);
+    Page<Product> findAllByBrand(Pageable pageable);
 
-    List<Product> findAllByBrandId(Brand brandId);
 }
