0f617bb8ddcdd119a0f05ca23a1f2809a59a5a40
FANG <107174112+Fangsangik@users.noreply.github.com>
2025-01-24 16:59:51 +0900

    refactor : "CartItemRequestDto" - 필드값 수정

--- src/main/java/com/Soo_Shinsa/cartitem/dto/CartItemRequestDto.java
+++ src/main/java/com/Soo_Shinsa/cartitem/dto/CartItemRequestDto.java
@@ -4,11 +4,19 @@ import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
-@NoArgsConstructor
 @Getter
+@NoArgsConstructor
 public class CartItemRequestDto {
-    @NotNull(message = "상품옵션은 필수값 입니다.")
-    private Long optionId;
-    @NotNull(message = "수량 필수값 입니다.")
+
+    @NotNull(message = "상품 아이디를 입력해주세요.")
+    private Long productId;
+
+    @NotNull(message = "수량을 입력해주세요.")
     private Integer quantity;
+
+    public CartItemRequestDto(Long productId, Integer quantity) {
+        this.productId = productId;
+        this.quantity = quantity;
+    }
 }
+
