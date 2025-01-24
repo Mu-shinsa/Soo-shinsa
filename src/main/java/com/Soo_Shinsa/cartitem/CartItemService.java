8d26e04c9f0cec246a9c2a36fbc18020ca38485c
FANG <107174112+Fangsangik@users.noreply.github.com>
2025-01-24 17:01:04 +0900

    refactor : "CartItemService" - Paging 처리 - User에 대한 권한 / 인증 정보 - delete void 처리

--- src/main/java/com/Soo_Shinsa/cartitem/CartItemService.java
+++ src/main/java/com/Soo_Shinsa/cartitem/CartItemService.java
@@ -1,17 +1,15 @@
 package com.Soo_Shinsa.cartitem;
 
-
+import com.Soo_Shinsa.cartitem.dto.CartItemRequestDto;
 import com.Soo_Shinsa.cartitem.dto.CartItemResponseDto;
-
-import java.util.List;
-
+import com.Soo_Shinsa.user.model.User;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 
 public interface CartItemService {
-    CartItemResponseDto create(Long optionId,Integer quantity,Long userId);
-    CartItemResponseDto findById(Long cartId, Long userId);
-    List<CartItemResponseDto> findByAll(Long userId);
-    CartItem findByIdOrElseThrow(Long id);
-    CartItemResponseDto update(Long cartId,Long userId,Integer quantity);
-
-    CartItemResponseDto delete(Long cartId,Long userId);
+    CartItemResponseDto create(User user, CartItemRequestDto requestDto);
+    CartItemResponseDto findById(Long cartId, User user);
+    CartItemResponseDto update(Long cartId, User user, Integer quantity);
+    Page<CartItemResponseDto> findByAll(Long userId, Pageable pageable);
+    void delete(Long cartId, User user);
 }
