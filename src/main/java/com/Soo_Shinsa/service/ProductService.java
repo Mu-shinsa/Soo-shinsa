e51ba2e7fa47d4fe3b82b4e1f7e16f59092f4b94
FANG <107174112+Fangsangik@users.noreply.github.com>
2025-01-24 12:37:48 +0900

    refactor : "ProductService" - Paging 처리 - findProduct FindProductResponseDto로 변경

--- src/main/java/com/Soo_Shinsa/service/ProductService.java
+++ src/main/java/com/Soo_Shinsa/service/ProductService.java
@@ -1,20 +1,26 @@
 package com.Soo_Shinsa.service;
 
+import com.Soo_Shinsa.dto.product.FindProductResponseDto;
 import com.Soo_Shinsa.dto.product.ProductRequestDto;
 import com.Soo_Shinsa.dto.product.ProductResponseDto;
 import com.Soo_Shinsa.model.User;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.web.multipart.MultipartFile;
 
-import java.util.List;
 
 public interface ProductService {
 
-    ProductResponseDto createProduct(User user, ProductRequestDto productRequestDto, Long brandId);
+    ProductResponseDto createProduct(User user, ProductRequestDto dto, Long brandId, MultipartFile imageFile);
 
-    ProductResponseDto updateProduct(User user, ProductRequestDto productRequestDto, Long productId);
+    ProductResponseDto updateProduct(User user, ProductRequestDto dto, Long productId, MultipartFile imageFile);
 
-    ProductResponseDto findProduct(Long productId);
+    FindProductResponseDto findProduct(Long productId);
 
-    List<ProductResponseDto> findProductListByBrandId(Long brandId);
+    Page<ProductResponseDto> findAllProductByBrandId(Long brandId, Pageable pageable);
 
-    List<ProductResponseDto> findAllProduct();
+    Page<ProductResponseDto> findAllProduct(Pageable pageable);
+
+    void deleteProduct(Long productId, User user);
 }
+
