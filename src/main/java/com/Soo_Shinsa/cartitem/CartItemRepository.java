394e22427af8777a0a184eb0884fd1521b32462d
FANG <107174112+Fangsangik@users.noreply.github.com>
2025-01-25 10:34:32 +0900

    refactor : "CartItemRepository" - findById default 처리

--- src/main/java/com/Soo_Shinsa/cartitem/CartItemRepository.java
+++ src/main/java/com/Soo_Shinsa/cartitem/CartItemRepository.java
@@ -1,5 +1,6 @@
 package com.Soo_Shinsa.cartitem;
 
+import com.Soo_Shinsa.cartitem.model.CartItem;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -8,6 +9,7 @@ public interface CartItemRepository extends JpaRepository<CartItem, Long> {
 
     Page<CartItem> findAllByUserUserId(Long userid, Pageable pageable);
 
-    //Order에서 Paging 처리 안되있어서 오류 발생
-    Page<CartItem> findByUserUserId(Long userId, Pageable pageable);
+    default CartItem findById(Long cartId, String exceptionMessage) {
+        return findById(cartId).orElseThrow(() -> new IllegalArgumentException(exceptionMessage));
+    }
 }
