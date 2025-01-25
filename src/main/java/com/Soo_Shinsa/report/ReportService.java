ab85efa3d2da639fefc94e1dd6e72625e709d423
FANG <107174112+Fangsangik@users.noreply.github.com>
2025-01-25 11:29:23 +0900

    refactor : "ReportService" - getAllReports : 모든 report 조회

--- src/main/java/com/Soo_Shinsa/report/ReportService.java
+++ src/main/java/com/Soo_Shinsa/report/ReportService.java
@@ -4,10 +4,12 @@ import com.Soo_Shinsa.report.dto.ReportProcessDto;
 import com.Soo_Shinsa.report.dto.ReportRequestDto;
 import com.Soo_Shinsa.report.dto.ReportResponseDto;
 import com.Soo_Shinsa.user.model.User;
+import org.springframework.data.domain.Page;
 
 public interface ReportService {
     ReportResponseDto createReport(ReportRequestDto requestDto, User user);
     void processReport(Long reportId, ReportProcessDto processDto, User user);
     ReportResponseDto getReport(Long reportId, User user);
     void deleteReport(Long reportId, User user);
+    Page<ReportResponseDto> getAllReports(User user, int page, int size);
 }
