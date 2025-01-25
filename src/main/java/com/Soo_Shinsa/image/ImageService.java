e1b9259bf937b33f8311baebbeca690795376e24
FANG <107174112+Fangsangik@users.noreply.github.com>
2025-01-25 13:38:03 +0900

    refactor : "ImageService" - uploadImage: 매개변수명 변경, targetId 추가

--- src/main/java/com/Soo_Shinsa/image/ImageService.java
+++ src/main/java/com/Soo_Shinsa/image/ImageService.java
@@ -3,7 +3,7 @@ package com.Soo_Shinsa.image;
 import org.springframework.web.multipart.MultipartFile;
 
 public interface ImageService {
-    Image uploadImage(MultipartFile file, String dirName);
+    Image uploadImage(MultipartFile file, String targetType, Long targetId);
     void deleteImage(String imageUrl);
     Image updateImage(MultipartFile newFile, String oldImageUrl, String dirName);
 }
