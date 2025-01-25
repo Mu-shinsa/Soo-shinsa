b50b32aa02c68cddb512ae12762db58eae74b8b7
FANG <107174112+Fangsangik@users.noreply.github.com>
2025-01-25 10:37:50 +0900

    refactor : "CartItemServiceImpl" - findById default 처리 - 사용자인지 검증

--- src/main/java/com/Soo_Shinsa/cartitem/CartItemServiceImpl.java
+++ src/main/java/com/Soo_Shinsa/cartitem/CartItemServiceImpl.java
@@ -2,13 +2,13 @@ package com.Soo_Shinsa.cartitem;
 
 import com.Soo_Shinsa.cartitem.dto.CartItemRequestDto;
 import com.Soo_Shinsa.cartitem.dto.CartItemResponseDto;
+import com.Soo_Shinsa.cartitem.model.CartItem;
 import com.Soo_Shinsa.product.ProductRepository;
 import com.Soo_Shinsa.product.model.Product;
 import com.Soo_Shinsa.product.model.ProductOption;
 import com.Soo_Shinsa.user.model.User;
 import com.Soo_Shinsa.product.ProductOptionRepository;
 import com.Soo_Shinsa.user.UserRepository;
-import jakarta.persistence.EntityNotFoundException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -46,25 +46,22 @@ public class CartItemServiceImpl implements CartItemService {
         return CartItemResponseDto.toDto(cartItem, productOptions);
     }
 
-    @Transactional(readOnly = true)
     @Override
     public CartItemResponseDto findById(Long cartId, User user) {
         // 사용자 정보 가져오기
         User userId = userRepository.findById(user.getUserId())
                 .orElseThrow(() -> new IllegalArgumentException("사용자를 찾을수 없습니다."));
 
-        CartItem cartItem = cartItemRepository.findById(cartId)
-                .orElseThrow(() -> new IllegalArgumentException("해당 id값이 존재하지 않습니다."));
+        CartItem cartItem = cartItemRepository.findById(cartId, "해당 id값이 존재하지 않습니다.");
 
         //사용자의 카트인지 확인
-        checkUser(cartItem, userId);
+        userId.validateCartItemUser(cartItem);
 
         List<ProductOption> productOptions = productOptionRepository.findAllByProductId(cartItem.getProduct().getId());
 
         return CartItemResponseDto.toDto(cartItem, productOptions);
     }
 
-    @Transactional(readOnly = true)
     @Override
     public Page<CartItemResponseDto> findByAll(Long userId, Pageable pageable) {
         User user = userRepository.findById(userId)
@@ -84,19 +81,17 @@ public class CartItemServiceImpl implements CartItemService {
         User userId = userRepository.findById(user.getUserId())
                 .orElseThrow(() -> new IllegalArgumentException("사용자를 찾을수 없습니다."));
 
-        CartItem cartItem = cartItemRepository.findById(cartId)
-                .orElseThrow(() -> new IllegalArgumentException("해당 id값이 존재하지 않습니다."));
+        CartItem cartItem = cartItemRepository.findById(cartId, "해당 id값이 존재하지 않습니다.");
 
-        checkUser(cartItem, userId);
+        userId.validateCartItemUser(cartItem);
 
         cartItem.updateCartItem(quantity);
 
-        CartItem saved = cartItemRepository.save(cartItem);
 
         // 상품 옵션 조회
-        List<ProductOption> productOptions = productOptionRepository.findAllByProductId(saved.getProduct().getId());
+        List<ProductOption> productOptions = productOptionRepository.findAllByProductId(cartItem.getProduct().getId());
 
-        return CartItemResponseDto.toDto(saved, productOptions);
+        return CartItemResponseDto.toDto(cartItem, productOptions);
     }
 
 
@@ -106,18 +101,10 @@ public class CartItemServiceImpl implements CartItemService {
         User userId = userRepository.findById(user.getUserId())
                 .orElseThrow(() -> new IllegalArgumentException("사용자를 찾을수 없습니다."));
 
-        CartItem cartItem = cartItemRepository.findById(cartId)
-                .orElseThrow(() -> new EntityNotFoundException("해당 id값이 존재하지 않습니다."));
+        CartItem cartItem = cartItemRepository.findById(cartId, "해당 id값이 존재하지 않습니다.");
 
-        checkUser(cartItem, userId);
+        userId.validateCartItemUser(cartItem);
 
         cartItemRepository.delete(cartItem);
-    }
-
-
-    private static void checkUser(CartItem cartItem, User userId) {
-        if (!cartItem.getUser().getUserId().equals(userId.getUserId())) {
-            throw new IllegalArgumentException("해당 사용자의 카트가 아닙니다.");
-        }
     }
 }
