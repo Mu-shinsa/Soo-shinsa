01c95068b1c9cba71029056a9aa1a571b9a95b90
haewook Lee <atolow@gmail.com>
2025-01-30 16:33:57 +0900

    feat: 예외처리 기능 추가

---
+++ src/main/java/com/Soo_Shinsa/exception/ErrorCode.java
@@ -0,0 +1,51 @@
+package com.Soo_Shinsa.exception;
+
+import lombok.Getter;
+import org.springframework.http.HttpStatus;
+
+@Getter
+public enum ErrorCode {
+
+    //InvalidInputException
+    //비밀번호가 틀렸을 때 출력하는 오류 메시지
+    WRONG_PASSWORD("비밀 번호가 틀렸습니다.", HttpStatus.BAD_REQUEST),
+    //아이디 비밀번호가 잘못됨
+    DIFFERENT_EMAIL_PASSWORD("이메일 혹은 비밀번호가 잘못되었습니다.", HttpStatus.BAD_REQUEST),
+    //탈퇴한 이메일로 가입을 시도할 때 출력하는 오류 메시지
+    EMAIL_DELETED("삭제된 이메일 입니다.", HttpStatus.BAD_REQUEST),
+    // 이미 삭제된 사용자가 조회 되었을 때 출력하는 오류 메시지
+    DELETED_USER("이미 삭제된 회원입니다.", HttpStatus.BAD_REQUEST),
+
+    // Exception
+    NOT_FOUND_USER("유저를 찾을 수 없습니다.", HttpStatus.NOT_FOUND),
+    NOT_FOUND_CART("카트를 찾을 수 없습니다.", HttpStatus.NOT_FOUND),
+    NOT_FOUND_PRODUCT("상품를 찾을 수 없습니다.", HttpStatus.NOT_FOUND),
+    NOT_FOUND_PRODUCTOPTION("상품옵션을 찾을 수 없습니다.", HttpStatus.NOT_FOUND),
+    NOT_FOUND_ORDER("오더를 찾을 수 없습니다.", HttpStatus.NOT_FOUND),
+    NOT_FOUND_ORDEROPTION("오더옵션을 찾을 수 없습니다.", HttpStatus.NOT_FOUND),
+
+    //잘못된 요청
+    WRONG_REQUEST("지원하지 않는 요청입니다.", HttpStatus.SERVICE_UNAVAILABLE),
+
+    // DuplicatedException
+    //중복된 이메일로 가일 할 때 출력하는 오류 메시지
+    EMAIL_EXIST("중복된 아이디 입니다.", HttpStatus.BAD_REQUEST),
+
+    // NoAuthorizedException
+    //로그인이 안 되었을 떄 출력하는 오류 메시지
+    NO_TOKEN("로그인이 필요합니다.", HttpStatus.UNAUTHORIZED),
+    //이미 로그인이 되어있을 때 출력하는 오류 메시지
+    ALREADY_LOGIN("이미 로그인이 되어있습니다.", HttpStatus.UNAUTHORIZED),
+    //권한이 없는 사용자가 수정, 삭제를 하려고 할 때
+    NO_AUTHOR_CHANGE("수정, 삭제는 작성자만 할 수 있습니다.", HttpStatus.UNAUTHORIZED),
+    NO_AUTHOR_READ_ONLY("읽기 전용 권한은 수정, 삭제를 할 수 없습니다.", HttpStatus.UNAUTHORIZED);
+
+
+    private final String message;
+    private final HttpStatus httpStatus;
+
+    ErrorCode(String message, HttpStatus httpStatus) {
+        this.message = message;
+        this.httpStatus = httpStatus;
+    }
+}
--- src/main/java/com/Soo_Shinsa/order/OrderItemRepository.java
+++ src/main/java/com/Soo_Shinsa/order/OrderItemRepository.java
@@ -1,13 +1,14 @@
 package com.Soo_Shinsa.order;
 
 
+import com.Soo_Shinsa.exception.ALLException;
 import com.Soo_Shinsa.order.model.OrderItem;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.http.HttpStatus;
-import org.springframework.web.server.ResponseStatusException;
+
+import static com.Soo_Shinsa.exception.ErrorCode.NOT_FOUND_ORDEROPTION;
 
 
 public interface OrderItemRepository extends JpaRepository<OrderItem, Long> {
@@ -23,7 +24,6 @@ public interface OrderItemRepository extends JpaRepository<OrderItem, Long> {
 
     default OrderItem findByIdOrElseThrow(Long orderItemId) {
         return findById(orderItemId).orElseThrow(
-                () -> new ResponseStatusException(HttpStatus.NOT_FOUND, "오더아이템를 찾을 수 없습니다")
-        );
+                () -> new ALLException(NOT_FOUND_ORDEROPTION));
     }
 }
