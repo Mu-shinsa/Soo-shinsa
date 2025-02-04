08ebc25e8d251c3ac78fd13518d5a21cbb12cd5e
FANG <107174112+Fangsangik@users.noreply.github.com>
2025-02-04 23:55:08 +0900

    refactor : "OrdersService" - 동적 쿼리 작성

--- src/main/java/com/Soo_Shinsa/order/OrdersServiceImpl.java
+++ src/main/java/com/Soo_Shinsa/order/OrdersServiceImpl.java
@@ -1,29 +1,34 @@
 package com.Soo_Shinsa.order;
 
-
 import com.Soo_Shinsa.cartitem.CartItemRepository;
 import com.Soo_Shinsa.cartitem.model.CartItem;
 import com.Soo_Shinsa.constant.OrdersStatus;
 import com.Soo_Shinsa.exception.NotFoundException;
+import com.Soo_Shinsa.order.dto.OrderDateRequestDto;
 import com.Soo_Shinsa.order.dto.OrdersResponseDto;
 import com.Soo_Shinsa.order.model.OrderItem;
 import com.Soo_Shinsa.order.model.Orders;
+import com.Soo_Shinsa.order.model.QOrders;
 import com.Soo_Shinsa.product.ProductRepository;
 import com.Soo_Shinsa.product.model.Product;
 import com.Soo_Shinsa.user.UserRepository;
 import com.Soo_Shinsa.user.model.User;
 import com.Soo_Shinsa.utils.EntityValidator;
+import com.querydsl.core.BooleanBuilder;
+import com.querydsl.jpa.impl.JPAQueryFactory;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.math.BigDecimal;
+import java.sql.Timestamp;
 import java.util.List;
 
 import static com.Soo_Shinsa.exception.ErrorCode.NOT_FOUND_CART;
-import static com.Soo_Shinsa.exception.ErrorCode.NOT_FOUND_ORDER;
 
 @Service
 @RequiredArgsConstructor
@@ -32,6 +37,7 @@ public class OrdersServiceImpl implements OrdersService {
     private final OrdersRepository ordersRepository;
     private final CartItemRepository cartItemRepository;
     private final UserRepository userRepository;
+    private final JPAQueryFactory queryFactory;
 
 
     @Override
@@ -40,39 +46,57 @@ public class OrdersServiceImpl implements OrdersService {
 
         Orders findOrder = ordersRepository.findByIdOrElseThrow(orderId);
 
-        EntityValidator.validateAndOrders(findOrder,findUser.getUserId());
+        EntityValidator.validateAndOrders(findOrder, findUser.getUserId());
 
 
         return OrdersResponseDto.toDto(findOrder);
     }
 
     @Override
-    public Page<OrdersResponseDto> getAllByUserId(User user, Pageable pageable) {
+    public Page<OrdersResponseDto> getAllByUserId(User user, OrderDateRequestDto dateRequestDto, int page, int size) {
 
+        QOrders orders = QOrders.orders;
+        Pageable pageable = PageRequest.of(page, size);
 
-        Page<Orders> findOrders = ordersRepository.findAllByUserUserId(user.getUserId(),pageable);
+        BooleanBuilder builder = new BooleanBuilder();
+        builder.and(orders.user.userId.eq(user.getUserId()));
 
-
-
-        if (findOrders == null) {
-            throw new NotFoundException(NOT_FOUND_ORDER);
+        if (dateRequestDto.getStartDate() != null) {
+            builder.and(orders.createdAt.goe(Timestamp.valueOf(dateRequestDto.getStartDate().atStartOfDay())));
         }
 
-        return findOrders.map(OrdersResponseDto::toDto);
+        if (dateRequestDto.getEndDate() != null) {
+            builder.and(orders.createdAt.loe(Timestamp.valueOf(dateRequestDto.getEndDate().atTime(23, 59, 59))));
+        }
+
+        List<Orders> findOrders = queryFactory.selectFrom(orders)
+                .where(builder)
+                .offset(pageable.getOffset())
+                .limit(pageable.getPageSize())
+                .fetch();
+
+        long total =
+                queryFactory.selectFrom(orders)
+                        .where(builder)
+                        .fetch()
+                        .size();
+
+        List<OrdersResponseDto> ordersResponseDtos =
+                findOrders.stream().map(OrdersResponseDto::toDto).toList();
+
+        return new PageImpl<>(ordersResponseDtos, pageable, total);
     }
 
 
-
-//    단일 상품 구매
+    //    단일 상품 구매
 //    상품을 찾아와서 주문번호를 생성 후 주문을 만들고 거기에 주문아이템에 물건을 담음
     @Transactional
     @Override
-    public OrdersResponseDto createSingleProductOrder(User user,Long productId, Integer quantity) {
+    public OrdersResponseDto createSingleProductOrder(User user, Long productId, Integer quantity) {
 
         Product product = productRepository.findByIdOrElseThrow(productId);
 
         Orders order = new Orders(product.getPrice().multiply(BigDecimal.valueOf(quantity)), OrdersStatus.BEFOREPAYMENT, user);
-
 
 
         OrderItem orderItem = new OrderItem(quantity, order, product);
@@ -117,12 +141,13 @@ public class OrdersServiceImpl implements OrdersService {
     @Override
     public OrdersResponseDto createOrder(User user) {
 
-        Orders order = new Orders(BigDecimal.ZERO, OrdersStatus.BEFOREPAYMENT,user);
+        Orders order = new Orders(BigDecimal.ZERO, OrdersStatus.BEFOREPAYMENT, user);
         Orders savedOrder = ordersRepository.save(order);
 
 
         return OrdersResponseDto.toDto(savedOrder);
     }
+
     @Transactional
     @Override
     public OrdersResponseDto updateOrder(User user, Long orderId, OrdersStatus status) {
@@ -130,7 +155,7 @@ public class OrdersServiceImpl implements OrdersService {
         User findUser = userRepository.findByIdOrElseThrow(user.getUserId());
         Orders findOrder = ordersRepository.findByIdOrElseThrow(orderId);
 
-        EntityValidator.validateAndOrders(findOrder,findUser.getUserId());
+        EntityValidator.validateAndOrders(findOrder, findUser.getUserId());
         findOrder.updateStatus(status);
         Orders savedOrder = ordersRepository.save(findOrder);
         return OrdersResponseDto.toDto(savedOrder);
