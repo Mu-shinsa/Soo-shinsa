3330216da8433fe5c9aa45690d14f027c2344638
FANG <107174112+Fangsangik@users.noreply.github.com>
2025-02-05 00:02:16 +0900

    refactor : "ReviewServiceImpl" - 동적 쿼리 처리

--- src/main/java/com/Soo_Shinsa/review/ReviewServiceImpl.java
+++ src/main/java/com/Soo_Shinsa/review/ReviewServiceImpl.java
@@ -5,19 +5,28 @@ import com.Soo_Shinsa.image.Image;
 import com.Soo_Shinsa.image.ImageService;
 import com.Soo_Shinsa.order.OrderItemRepository;
 import com.Soo_Shinsa.order.model.OrderItem;
+import com.Soo_Shinsa.product.ProductRepository;
+import com.Soo_Shinsa.product.model.Product;
+import com.Soo_Shinsa.review.dto.ReviewRateDto;
 import com.Soo_Shinsa.review.dto.ReviewRequestDto;
 import com.Soo_Shinsa.review.dto.ReviewResponseDto;
 import com.Soo_Shinsa.review.dto.ReviewUpdateDto;
+import com.Soo_Shinsa.review.model.QReview;
 import com.Soo_Shinsa.review.model.Review;
 import com.Soo_Shinsa.user.model.User;
 import com.Soo_Shinsa.utils.EntityValidator;
+import com.querydsl.core.BooleanBuilder;
+import com.querydsl.jpa.impl.JPAQueryFactory;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.multipart.MultipartFile;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
@@ -26,6 +35,8 @@ public class ReviewServiceImpl implements ReviewService {
     private final ReviewRepository reviewRepository;
     private final OrderItemRepository orderItemRepository;
     private final ImageService imageService;
+    private final ProductRepository productRepository;
+    private final JPAQueryFactory queryFactory;
 
     /**
      * 리뷰 생성
@@ -102,19 +113,51 @@ public class ReviewServiceImpl implements ReviewService {
     }
 
     /**
-     * 상품 리뷰 조회
+     * 상품 리뷰 조회 (별점 순)
      *
      * @param productId
      * @param productId 상품 ID
+     * @param reviewRateDto 리뷰 별점 필터링 DTO
      * @param page      페이지 번호
      * @param size      페이지 크기
      * @return reviews.map(ReviewResponseDto : : toDto);
      */
-    public Page<ReviewResponseDto> getReviewsByProductId(Long productId, int page, int size) {
+    public Page<ReviewResponseDto> getReviewsByProductId(Long productId, ReviewRateDto reviewRateDto, int page, int size) {
         // Pageable 객체를 Service에서 생성
+        Product product = productRepository.findByIdOrElseThrow(productId);
+        QReview review = QReview.review;
         Pageable pageable = PageRequest.of(page, size);
-        Page<Review> reviews = reviewRepository.findAllByProductId(productId, pageable);
-        return reviews.map(ReviewResponseDto::toDto);
+
+        BooleanBuilder builder = new BooleanBuilder();
+        builder.and(review.product.id.eq(productId));
+
+        if (reviewRateDto.getMinRate() != null) {
+            builder.and(review.rate.goe(reviewRateDto.getMinRate()));
+        }
+
+        if (reviewRateDto.getMaxRate() != null) {
+            builder.and(review.rate.loe(reviewRateDto.getMaxRate()));
+        }
+
+        List<Review> reviews = queryFactory
+                .selectFrom(review)
+                .where(builder)
+                .orderBy(review.rate.desc())
+                .offset(pageable.getOffset())
+                .limit(pageable.getPageSize())
+                .fetch();
+
+        long total = queryFactory
+                .selectFrom(review)
+                .where(review.product.id.eq(productId))
+                .fetch()
+                .size();
+
+        List<ReviewResponseDto> reviewResponseDtos = reviews.stream()
+                .map(ReviewResponseDto::toDto)
+                .toList();
+
+        return new PageImpl<>(reviewResponseDtos, pageable, total);
     }
 
 
