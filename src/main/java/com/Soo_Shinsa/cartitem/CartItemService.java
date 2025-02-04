a7f2ce8a160c500e3b7623e5a7da7c3d5e131079
FANG <107174112+Fangsangik@users.noreply.github.com>
2025-02-04 23:50:36 +0900

    refactor : "CartItemService" - 동적 쿼리 작성

--- src/main/java/com/Soo_Shinsa/cartitem/CartItemService.java
+++ src/main/java/com/Soo_Shinsa/cartitem/CartItemService.java
@@ -1,5 +1,6 @@
 package com.Soo_Shinsa.cartitem;
 
+import com.Soo_Shinsa.cartitem.dto.CartItemDateRequestDto;
 import com.Soo_Shinsa.cartitem.dto.CartItemRequestDto;
 import com.Soo_Shinsa.cartitem.dto.CartItemResponseDto;
 import com.Soo_Shinsa.user.model.User;
@@ -9,6 +10,6 @@ public interface CartItemService {
     CartItemResponseDto create(User user, CartItemRequestDto requestDto);
     CartItemResponseDto findById(Long cartId, User user);
     CartItemResponseDto update(Long cartId, User user, Integer quantity);
-    Page<CartItemResponseDto> findByAll(User user, int page, int size);
+    Page<CartItemResponseDto> findByAll(User user, CartItemDateRequestDto requestDto, int page, int size);
     void delete(Long cartId, User user);
 }
