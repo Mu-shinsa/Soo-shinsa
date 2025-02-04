f513da054cdabb5ed50f20af8dd4d8eea918d8c9
FANG <107174112+Fangsangik@users.noreply.github.com>
2025-02-04 23:58:29 +0900

    refactor : "ProductResponseDto" - categoryId 추가

--- src/main/java/com/Soo_Shinsa/product/dto/ProductResponseDto.java
+++ src/main/java/com/Soo_Shinsa/product/dto/ProductResponseDto.java
@@ -2,6 +2,7 @@ package com.Soo_Shinsa.product.dto;
 
 import com.Soo_Shinsa.constant.ProductStatus;
 import com.Soo_Shinsa.product.model.Product;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 
@@ -16,14 +17,17 @@ public class ProductResponseDto {
     private BigDecimal price;
     private ProductStatus status;
     private Long brandId;
+    private Long categoryId;
     private String imageUrl;
 
-    public ProductResponseDto(Long id, String name, BigDecimal price, ProductStatus status, Long brandId, String imageUrl) {
+    @Builder
+    public ProductResponseDto(Long id, String name, BigDecimal price, ProductStatus status, Long brandId, Long categoryId, String imageUrl) {
         this.id = id;
         this.name = name;
         this.price = price;
         this.status = status;
         this.brandId = brandId;
+        this.categoryId = categoryId;
         this.imageUrl = imageUrl;
     }
 
@@ -34,6 +38,7 @@ public class ProductResponseDto {
                 product.getPrice(),
                 product.getProductStatus(),
                 product.getBrand().getId(),
+                product.getCategory().getId(),
                 product.getImageUrl()
         );
     }
