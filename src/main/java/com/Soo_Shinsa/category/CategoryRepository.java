edea0104cfd565ca9b70ac3ba789c039d64b5d42
FANG <107174112+Fangsangik@users.noreply.github.com>
2025-02-04 23:52:19 +0900

    refactor : "CategoryRepository" - 동적 쿼리 작성

--- src/main/java/com/Soo_Shinsa/category/CategoryRepository.java
+++ src/main/java/com/Soo_Shinsa/category/CategoryRepository.java
@@ -2,21 +2,11 @@ package com.Soo_Shinsa.category;
 
 import com.Soo_Shinsa.category.model.Category;
 import com.Soo_Shinsa.exception.NotFoundException;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-
-import java.util.List;
 
 import static com.Soo_Shinsa.exception.ErrorCode.NOT_FOUND_CATEGORY;
 
 public interface CategoryRepository extends JpaRepository<Category, Long> {
-
-    @Query("SELECT c FROM Category c")
-    Page<Category> findAll(Pageable pageable);
-
-    List<Category> findByBrandId(Long brandId);
 
     default Category findByIdOrElseThrow(Long categoryId) {
         return findById(categoryId).orElseThrow(
