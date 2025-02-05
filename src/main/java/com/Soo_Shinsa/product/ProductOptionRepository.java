8312993af0159927748c9c60289712f89bbe37e6
FANG <107174112+Fangsangik@users.noreply.github.com>
2025-02-05 11:31:00 +0900

    refactor : "ProductOptionRepository" - 동적쿼리 리포로 뺌

--- src/main/java/com/Soo_Shinsa/product/ProductOptionRepository.java
+++ src/main/java/com/Soo_Shinsa/product/ProductOptionRepository.java
@@ -2,22 +2,13 @@ package com.Soo_Shinsa.product;
 
 import com.Soo_Shinsa.exception.NotFoundException;
 import com.Soo_Shinsa.product.model.ProductOption;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
 import static com.Soo_Shinsa.exception.ErrorCode.NOT_FOUND_PRODUCT_OPTION;
 
-public interface ProductOptionRepository extends JpaRepository<ProductOption, Long> {
-    @Query("SELECT po FROM ProductOption po WHERE " +
-            "(:size IS NULL OR po.size = :size) AND " +
-            "(:color IS NULL OR po.color = :color)")
-    Page<ProductOption> findProductsByOptionalSizeAndColor(@Param("size") String size, @Param("color") String color, Pageable pageable);
-
+public interface ProductOptionRepository extends JpaRepository<ProductOption, Long>, ProductOptionCustomRepository {
     List<ProductOption> findProductOptionByProductId(Long productId);
 
     void deleteAllByProductId(Long productId);
