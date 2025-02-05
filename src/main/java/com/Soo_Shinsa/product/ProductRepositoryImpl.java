52138b8f2d3e847cfd79c6477ca07637c8715188
FANG <107174112+Fangsangik@users.noreply.github.com>
2025-02-05 11:32:04 +0900

    feat : "ProductRepositoryImpl" - 동적쿼리 리포로 뺌

---
+++ src/main/java/com/Soo_Shinsa/product/ProductRepositoryImpl.java
@@ -0,0 +1,69 @@
+package com.Soo_Shinsa.product;
+
+import com.Soo_Shinsa.category.model.QCategory;
+import com.Soo_Shinsa.product.dto.FindProductRequestDto;
+import com.Soo_Shinsa.product.dto.ProductResponseDto;
+import com.Soo_Shinsa.product.model.QProduct;
+import com.querydsl.core.BooleanBuilder;
+import com.querydsl.core.types.Projections;
+import com.querydsl.core.types.dsl.Expressions;
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.Pageable;
+
+@RequiredArgsConstructor
+public class ProductRepositoryImpl implements ProductCustomRepository {
+
+    private final JPAQueryFactory queryFactory;
+
+    @Override
+    public Page<ProductResponseDto> findAllProduct(Long brandId, FindProductRequestDto requestDto, Pageable pageable) {
+        QProduct product = QProduct.product;
+        QCategory category = QCategory.category;
+
+        BooleanBuilder builder = new BooleanBuilder();
+        builder.and(product.brand.id.eq(brandId));
+
+        if (requestDto.getNameKeyword() != null && !requestDto.getNameKeyword().isEmpty()) {
+            builder.and(product.name.containsIgnoreCase(requestDto.getNameKeyword()));
+        }
+
+        if (requestDto.getMinPrice() != null) {
+            builder.and(product.price.goe(requestDto.getMinPrice()));
+        }
+
+        if (requestDto.getMaxPrice() != null) {
+            builder.and(product.price.loe(requestDto.getMaxPrice()));
+        }
+
+        return new PageImpl<>(
+                queryFactory
+                        .select(Projections.constructor(ProductResponseDto.class,
+                                product.id,
+                                product.name,
+                                product.price,
+                                product.imageUrl,
+                                product.productStatus,
+                                product.brand.id,
+                                product.category.id,
+                                Expressions.numberTemplate(Long.class, "COUNT(*) OVER()")
+                        ))
+                        .from(product)
+                        .leftJoin(category).on(product.category.id.eq(category.id))
+                        .where(builder)
+                        .orderBy(product.price.desc())
+                        .offset(pageable.getOffset())
+                        .limit(pageable.getPageSize())
+                        .fetch(),
+                pageable,
+                queryFactory
+                        .select(product.count())
+                        .from(product)
+                        .where(builder)
+                        .fetch()
+                        .size()
+        );
+    }
+}
