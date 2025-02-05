1a8a39cf19f93e77e84dde8da0d434609fdd24ad
FANG <107174112+Fangsangik@users.noreply.github.com>
2025-02-05 11:19:31 +0900

    feat : "BrandServiceImpl" - ServiceCode에 있던 동적 쿼리 리포로 변경

--- src/main/java/com/Soo_Shinsa/brand/BrandServiceImpl.java
+++ src/main/java/com/Soo_Shinsa/brand/BrandServiceImpl.java
@@ -7,11 +7,8 @@ import com.Soo_Shinsa.brand.dto.FindBrandAllResponseDto;
 import com.Soo_Shinsa.constant.BrandStatus;
 import com.Soo_Shinsa.user.model.User;
 import com.Soo_Shinsa.utils.EntityValidator;
-import com.querydsl.core.BooleanBuilder;
-import com.querydsl.jpa.impl.JPAQueryFactory;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
@@ -24,7 +21,6 @@ import java.util.List;
 public class BrandServiceImpl implements BrandService {
 
     private final BrandRepository brandRepository;
-    private final JPAQueryFactory queryFactory;
 
     @Transactional
     @Override
@@ -50,7 +46,7 @@ public class BrandServiceImpl implements BrandService {
 
         EntityValidator.validateAdminOrVendorAccess(user);
         Brand findBrand = brandRepository.findByIdOrElseThrow(brandId);
-        findBrand.update(dto.getRegistrationNum(),dto.getName(),dto.getContext(), dto.getStatus());
+        findBrand.update(dto.getRegistrationNum(), dto.getName(), dto.getContext(), dto.getStatus());
 
         return BrandUpdateResponseDto.toDto(findBrand);
     }
@@ -75,30 +71,8 @@ public class BrandServiceImpl implements BrandService {
 
     @Override
     public Page<FindBrandAllResponseDto> getAll(int page, int size) {
-        QBrand brand = QBrand.brand;
         Pageable pageable = PageRequest.of(page, size);
-
-        // 동적 조건 생성
-        BooleanBuilder builder = new BooleanBuilder();
-
-        // QueryDSL로 브랜드 목록 조회
-        List<Brand> brands = queryFactory.selectFrom(brand)
-                .where(builder)
-                .orderBy(brand.name.asc())
-                .fetch();
-
-        // 총 개수 조회
-        long total = queryFactory.select(brand.count())
-                .from(brand)
-                .where(builder)
-                .fetch()
-                .size();
-
-        List<FindBrandAllResponseDto> findBrandAllDtos = brands.stream()
-                .map(FindBrandAllResponseDto::of)  // 반환 타입에 맞는 DTO로 변환
-                .toList();
-
-        return new PageImpl<>(findBrandAllDtos, pageable, total);
+        return brandRepository.getAllBrand(pageable);
     }
 
 }
