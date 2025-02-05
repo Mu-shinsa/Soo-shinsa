aa4fb699c93c62e18ee6f628b0ba5fa60c5b6c0e
FANG <107174112+Fangsangik@users.noreply.github.com>
2025-02-05 11:29:39 +0900

    refactor : "OrdersServiceImpl" - 동적쿼리 리포로 뺌

--- src/main/java/com/Soo_Shinsa/order/OrdersServiceImpl.java
+++ src/main/java/com/Soo_Shinsa/order/OrdersServiceImpl.java
@@ -8,24 +8,19 @@ import com.Soo_Shinsa.order.dto.OrderDateRequestDto;
 import com.Soo_Shinsa.order.dto.OrdersResponseDto;
 import com.Soo_Shinsa.order.model.OrderItem;
 import com.Soo_Shinsa.order.model.Orders;
-import com.Soo_Shinsa.order.model.QOrders;
 import com.Soo_Shinsa.product.ProductRepository;
 import com.Soo_Shinsa.product.model.Product;
 import com.Soo_Shinsa.user.UserRepository;
 import com.Soo_Shinsa.user.model.User;
 import com.Soo_Shinsa.utils.EntityValidator;
-import com.querydsl.core.BooleanBuilder;
-import com.querydsl.jpa.impl.JPAQueryFactory;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.math.BigDecimal;
-import java.sql.Timestamp;
 import java.util.List;
 
 import static com.Soo_Shinsa.exception.ErrorCode.NOT_FOUND_CART;
@@ -37,7 +32,6 @@ public class OrdersServiceImpl implements OrdersService {
     private final OrdersRepository ordersRepository;
     private final CartItemRepository cartItemRepository;
     private final UserRepository userRepository;
-    private final JPAQueryFactory queryFactory;
 
 
     @Override
@@ -54,37 +48,8 @@ public class OrdersServiceImpl implements OrdersService {
 
     @Override
     public Page<OrdersResponseDto> getAllByUserId(User user, OrderDateRequestDto dateRequestDto, int page, int size) {
-
-        QOrders orders = QOrders.orders;
         Pageable pageable = PageRequest.of(page, size);
-
-        BooleanBuilder builder = new BooleanBuilder();
-        builder.and(orders.user.userId.eq(user.getUserId()));
-
-        if (dateRequestDto.getStartDate() != null) {
-            builder.and(orders.createdAt.goe(Timestamp.valueOf(dateRequestDto.getStartDate().atStartOfDay())));
-        }
-
-        if (dateRequestDto.getEndDate() != null) {
-            builder.and(orders.createdAt.loe(Timestamp.valueOf(dateRequestDto.getEndDate().atTime(23, 59, 59))));
-        }
-
-        List<Orders> findOrders = queryFactory.selectFrom(orders)
-                .where(builder)
-                .offset(pageable.getOffset())
-                .limit(pageable.getPageSize())
-                .fetch();
-
-        long total =
-                queryFactory.selectFrom(orders)
-                        .where(builder)
-                        .fetch()
-                        .size();
-
-        List<OrdersResponseDto> ordersResponseDtos =
-                findOrders.stream().map(OrdersResponseDto::toDto).toList();
-
-        return new PageImpl<>(ordersResponseDtos, pageable, total);
+        return ordersRepository.getAllByUserId(user, dateRequestDto, pageable);
     }
 
 
