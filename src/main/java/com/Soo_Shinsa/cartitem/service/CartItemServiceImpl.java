b4331f58afee4aabaf96b7594f5f809dfe225307
FANG <107174112+Fangsangik@users.noreply.github.com>
2025-02-06 15:30:26 +0900

    refactor : "CartItemServiceImpl" - applyCartItem 추가

---
+++ src/main/java/com/Soo_Shinsa/cartitem/service/CartItemServiceImpl.java
@@ -0,0 +1,206 @@
+package com.Soo_Shinsa.cartitem.service;
+
+import com.Soo_Shinsa.cartitem.dto.*;
+import com.Soo_Shinsa.cartitem.model.CartItem;
+import com.Soo_Shinsa.cartitem.repository.CartItemRepository;
+import com.Soo_Shinsa.constant.ProductStatus;
+import com.Soo_Shinsa.coupon.calculate.DiscountCouponCalculator;
+import com.Soo_Shinsa.coupon.calculate.PercentageDiscountCalculator;
+import com.Soo_Shinsa.coupon.model.Coupon;
+import com.Soo_Shinsa.coupon.model.CouponUser;
+import com.Soo_Shinsa.coupon.repository.CouponRepository;
+import com.Soo_Shinsa.coupon.repository.CouponUserRepository;
+import com.Soo_Shinsa.exception.ErrorCode;
+import com.Soo_Shinsa.exception.InternalServerException;
+import com.Soo_Shinsa.exception.InvalidInputException;
+import com.Soo_Shinsa.product.model.Product;
+import com.Soo_Shinsa.product.model.ProductOption;
+import com.Soo_Shinsa.product.repository.ProductOptionRepository;
+import com.Soo_Shinsa.product.repository.ProductRepository;
+import com.Soo_Shinsa.user.UserRepository;
+import com.Soo_Shinsa.user.model.User;
+import com.Soo_Shinsa.utils.EntityValidator;
+import lombok.RequiredArgsConstructor;
+import org.redisson.api.RLock;
+import org.redisson.api.RedissonClient;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.math.BigDecimal;
+import java.util.List;
+import java.util.Optional;
+import java.util.concurrent.TimeUnit;
+
+@Service
+@RequiredArgsConstructor
+public class CartItemServiceImpl implements CartItemService {
+
+    private final CartItemRepository cartItemRepository;
+    private final ProductOptionRepository productOptionRepository;
+    private final UserRepository userRepository;
+    private final ProductRepository productRepository;
+    private final CouponUserRepository couponUserRepository;
+    private final CouponRepository couponRepository;
+    private final RedissonClient redissonClient;
+
+    @Transactional
+    @Override
+    public CartItemResponseDto create(User user, CartItemRequestDto requestDto) {
+
+        Product product = productRepository.findByIdOrElseThrow(requestDto.getProductId());
+
+        if (product.getProductStatus().equals(ProductStatus.SOLD_OUT) || product.getProductStatus().equals(ProductStatus.UNAVAILABLE)) {
+            throw new InternalServerException(ErrorCode.CAN_NOT_USE_PRODUCT);
+        }
+
+        List<ProductOption> productOptions = productOptionRepository.findProductOptionByProductId(product.getId());
+
+        CartItem cartItem = CartItem.builder()
+                .quantity(requestDto.getQuantity())
+                .user(user)
+                .product(product)
+                .productOption(productOptions.get(0))
+                .build();
+
+        cartItemRepository.save(cartItem);
+
+        return CartItemResponseDto.toDto(cartItem, productOptions);
+    }
+
+    @Override
+    public CartItemResponseDto findById(Long cartId, User user) {
+        // 사용자 정보 가져오기
+        User userId = userRepository.findByIdOrElseThrow(user.getUserId());
+
+        CartItem cartItem = cartItemRepository.findByIdOrElseThrow(cartId);
+
+        //사용자의 카트인지 확인
+        EntityValidator.validateUserOwnership(userId.getUserId(), cartItem.getUser().getUserId());
+
+        List<ProductOption> productOptions = productOptionRepository.findProductOptionByProductId(cartItem.getProduct().getId());
+
+        return CartItemResponseDto.toDto(cartItem, productOptions);
+    }
+
+    @Override
+    public Page<CartItemResponseDto> findByAll(User user, CartItemDateRequestDto requestDto, int page, int size) {
+        Pageable pageable = PageRequest.of(page, size);
+        return cartItemRepository.findByAllCartItem(user, requestDto, pageable);
+    }
+
+
+    @Transactional
+    @Override
+    public CartItemResponseDto update(Long cartId, User user, Integer quantity) {
+        User userId = userRepository.findByIdOrElseThrow(user.getUserId());
+
+        CartItem cartItem = cartItemRepository.findByIdOrElseThrow(cartId);
+
+        EntityValidator.validateUserOwnership(userId.getUserId(), cartItem.getUser().getUserId());
+        cartItem.updateCartItem(quantity);
+
+
+        // 상품 옵션 조회
+        List<ProductOption> productOptions = productOptionRepository.findProductOptionByProductId(cartItem.getProduct().getId());
+
+        return CartItemResponseDto.toDto(cartItem, productOptions);
+    }
+
+    @Transactional
+    public ApplyCouponCartResponseDto applyCoupon(Long cartId, ApplyCouponCartRequestDto requestDto, User user) {
+        CartItem cartItem = cartItemRepository.findByIdOrElseThrow(cartId);
+
+        String lockKey = "coupon-lock:" + requestDto.getCouponId();
+        RLock lock = redissonClient.getLock(lockKey);
+
+        try {
+            if (!lock.tryLock(5, 10, TimeUnit.SECONDS)) {
+                throw new IllegalStateException("현재 쿠폰 사용 요청이 많아 잠시 후 다시 시도해주세요.");
+            }
+
+            Optional<CouponUser> existingCouponUser = couponUserRepository.findByCouponIdAndUserUserId(requestDto.getCouponId(), user.getUserId());
+            if (existingCouponUser.isPresent() && existingCouponUser.get().isUsed()) {
+                throw new InvalidInputException(ErrorCode.ALREADY_USED_COUPON);
+            }
+
+            // 사용되지 않은 쿠폰이 있는지 확인
+
+            Coupon coupon;
+            CouponUser couponUser;
+
+            Optional<CouponUser> optionalCouponUser = couponUserRepository.findUnusedCouponByCouponId(requestDto.getCouponId());
+            if (optionalCouponUser.isPresent()) {
+                // 기존에 사용하지 않은 쿠폰이 있으면 그대로 사용
+                couponUser = optionalCouponUser.get();
+                coupon = couponUser.getCoupon();
+
+            } else {
+                // 기존 쿠폰이 없지만 maxCount가 남아 있다면 새로운 CouponUser 생성
+                coupon = couponRepository.findById(requestDto.getCouponId())
+                        .orElseThrow(() -> new InvalidInputException(ErrorCode.NOT_FOUND_COUPON));
+
+                if (coupon.getMaxCount() <= 0) {
+                    throw new InvalidInputException(ErrorCode.COUPON_OUT_OF_STOCK);
+                }
+
+                // 새로운 CouponUser 발급
+                couponUser = CouponUser.builder()
+                        .coupon(coupon)
+                        .user(user)
+                        .isUsed(false)
+                        .usedAt(null)
+                        .build();
+
+                couponUserRepository.save(couponUser);
+            }
+
+            // 쿠폰이 만료되었는지 확인
+            if (coupon.isExpired()) {
+                throw new InvalidInputException(ErrorCode.EXPIRED_COUPON);
+            }
+
+            // 할인 가격 계산
+            DiscountCouponCalculator discountCouponCalculator = new PercentageDiscountCalculator();
+            BigDecimal discountPrice = discountCouponCalculator.calculateDiscountedPrice(cartItem.getProduct().getPrice(), coupon.getDiscountRate());
+
+            // 장바구니 아이템에 쿠폰 적용
+            cartItem.applyCoupon(coupon, discountPrice);
+            cartItemRepository.save(cartItem);
+
+            // 쿠폰 사용 처리
+            couponUser.markAsUsed();
+            coupon.decreaseMaxCount(1);  // 재고 감소
+            couponUserRepository.save(couponUser);
+
+            // 응답 DTO 반환
+            List<ProductOption> productOptions = productOptionRepository.findProductOptionByProductId(cartItem.getProduct().getId());
+            return ApplyCouponCartResponseDto.toDto(cartItem, productOptions);
+
+        } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
+            throw new IllegalStateException("쿠폰 적용 중 오류가 발생했습니다.", e);
+        } finally {
+            if (lock.isHeldByCurrentThread()) {
+                lock.unlock();
+            }
+        }
+    }
+
+
+
+
+    @Transactional
+    @Override
+    public void delete(Long cartId, User user) {
+        User userId = userRepository.findByIdOrElseThrow(user.getUserId());
+
+        CartItem cartItem = cartItemRepository.findByIdOrElseThrow(cartId);
+
+        EntityValidator.validateUserOwnership(userId.getUserId(), cartItem.getUser().getUserId());
+
+        cartItemRepository.delete(cartItem);
+    }
+}
