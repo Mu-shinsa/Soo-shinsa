f10f3e1a4c79259f965a433432047adf01b1cef8
haewook Lee <atolow@gmail.com>
2025-02-06 20:45:14 +0900

    fix:데드락 문제 발생해서 락 생성

--- src/main/java/com/Soo_Shinsa/order/service/OrderItemServiceImpl.java
+++ src/main/java/com/Soo_Shinsa/order/service/OrderItemServiceImpl.java
@@ -17,11 +17,15 @@ import com.Soo_Shinsa.user.repository.UserRepository;
 import com.Soo_Shinsa.utils.EntityValidator;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.redisson.api.RLock;
+import org.redisson.api.RedissonClient;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.concurrent.TimeUnit;
 
 import static com.Soo_Shinsa.constant.OrdersStatus.BEFOREPAYMENT;
 import static com.Soo_Shinsa.exception.ErrorCode.ONLY_BEFORE_PAYMENT;
@@ -34,11 +38,20 @@ public class OrderItemServiceImpl implements OrderItemService {
     private final ProductRepository productRepository;
     private final OrdersRepository ordersRepository;
     private final UserRepository userRepository;
+    private final RedissonClient redissonClient;
 
     //오더 아이템 생성
     @Transactional
     @Override
     public OrderItemResponseDto createOrderItem(OrderItemRequestDto requestDto, User user) {
+
+        String lockKey = "lock:orderItem:" + requestDto.getOrderId(); // 쿠폰 ID 기반 락
+        RLock lock = redissonClient.getLock(lockKey);
+
+        try {
+            if (!lock.tryLock(10, 30, TimeUnit.SECONDS)) {
+                throw new IllegalStateException("현재 오더아이템 발급 요청이 많아 잠시 후 다시 시도해주세요.");
+            }
 
         User findUser = userRepository.findByIdOrElseThrow(user.getUserId());
         Orders findOrder = ordersRepository.findByIdOrElseThrow(requestDto.getOrderId());
@@ -69,6 +82,14 @@ public class OrderItemServiceImpl implements OrderItemService {
         ordersRepository.save(findOrder);
 
         return OrderItemResponseDto.toDto(savedOrderItem);
+        } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
+            throw new IllegalStateException("오더아이템 생성 중 오류가 발생했습니다.", e);
+        } finally {
+            if (lock.isHeldByCurrentThread()) {
+                lock.unlock();
+            }
+        }
     }
 
     @Override
