e5ca27359a887e40d025cfef980e34baf9796f7a
FANG <107174112+Fangsangik@users.noreply.github.com>
2025-02-06 15:28:54 +0900

    refactor : "CartItem" - 쿠폰과 연관관계 설정

--- src/main/java/com/Soo_Shinsa/cartitem/model/CartItem.java
+++ src/main/java/com/Soo_Shinsa/cartitem/model/CartItem.java
@@ -1,6 +1,7 @@
 package com.Soo_Shinsa.cartitem.model;
 
 import com.Soo_Shinsa.constant.BaseTimeEntity;
+import com.Soo_Shinsa.coupon.model.Coupon;
 import com.Soo_Shinsa.product.model.Product;
 import com.Soo_Shinsa.product.model.ProductOption;
 import com.Soo_Shinsa.user.model.User;
@@ -8,6 +9,8 @@ import jakarta.persistence.*;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
 
 @Entity
 @Getter
@@ -33,15 +36,21 @@ public class CartItem extends BaseTimeEntity {
     @JoinColumn(name = "product_id", nullable = false)
     private Product product;
 
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "coupon_id")
+    private Coupon coupon;
+
+    private BigDecimal discountedPrice;
 
     @Builder
-    public CartItem(Integer quantity, User user, ProductOption productOption, Product product) {
+    public CartItem(Integer quantity, User user, ProductOption productOption, Product product, Coupon coupon, BigDecimal discountedPrice) {
         this.quantity = quantity;
         this.user = user;
         this.productOption = productOption;
         this.product = product;
+        this.coupon = coupon;
+        this.discountedPrice = discountedPrice;
     }
-
 
     public void updateCartItem(Integer quantity) {
         if (quantity < 1) {
@@ -49,5 +58,10 @@ public class CartItem extends BaseTimeEntity {
         }
 
         this.quantity = quantity;
+    }
+
+    public void applyCoupon(Coupon coupon, BigDecimal discountedPrice) {
+        this.coupon = coupon; // 쿠폰 정보 저장
+        this.discountedPrice = discountedPrice; // 할인된 가격 저장
     }
 }
