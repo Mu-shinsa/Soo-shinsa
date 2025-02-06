d3ba670f70e4c79701c82033d5d22e3db04d10e5
FANG <107174112+Fangsangik@users.noreply.github.com>
2025-02-06 15:36:39 +0900

    feat : "RedisConfig" - 레디스 설정

---
+++ src/main/java/com/Soo_Shinsa/config/RedisConfig.java
@@ -0,0 +1,21 @@
+package com.Soo_Shinsa.config;
+
+import org.redisson.Redisson;
+import org.redisson.api.RedissonClient;
+import org.redisson.config.Config;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class RedisConfig {
+
+    @Bean
+    public RedissonClient redissonClient() {
+        Config config = new Config();
+        config.useSingleServer().setAddress("redis://localhost:6379")
+                .setConnectionMinimumIdleSize(10)
+                .setConnectionPoolSize(64);
+
+        return Redisson.create(config);
+    }
+}
