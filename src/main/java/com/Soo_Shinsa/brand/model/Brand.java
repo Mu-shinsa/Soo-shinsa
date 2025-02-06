5d2c2e5d1a9642004d121af81ed60ee09bd217be
FANG <107174112+Fangsangik@users.noreply.github.com>
2025-02-06 15:26:58 +0900

    refactor : "Brand" - 브랜드에서 쿠폰 발행

--- src/main/java/com/Soo_Shinsa/brand/model/Brand.java
+++ src/main/java/com/Soo_Shinsa/brand/model/Brand.java
@@ -1,10 +1,10 @@
-package com.Soo_Shinsa.brand;
+package com.Soo_Shinsa.brand.model;
 
 import com.Soo_Shinsa.constant.BaseTimeEntity;
 import com.Soo_Shinsa.constant.BrandStatus;
+import com.Soo_Shinsa.coupon.model.CouponBrandRelation;
 import com.Soo_Shinsa.user.model.User;
 import jakarta.persistence.*;
-import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -12,14 +12,17 @@ import org.hibernate.annotations.DynamicInsert;
 import org.hibernate.annotations.DynamicUpdate;
 import org.hibernate.validator.constraints.Length;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import static com.Soo_Shinsa.constant.BrandStatus.APPLY;
 
 @Entity
 @DynamicInsert
 @DynamicUpdate
 @Getter
-@NoArgsConstructor(access = AccessLevel.PROTECTED)
-public class Brand  extends BaseTimeEntity {
+@NoArgsConstructor
+public class Brand extends BaseTimeEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -44,19 +47,29 @@ public class Brand  extends BaseTimeEntity {
     @JoinColumn(name = "user_id", nullable = false)
     private User user;
 
+    @OneToMany(mappedBy = "brand", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<CouponBrandRelation> couponBrandRelations = new ArrayList<>();
+
+    private Integer couponCount;
+    private Boolean isCouponLimited = false;
+
     @Builder
-    public Brand(String registrationNum, String name, String context, BrandStatus status, User user) {
+    public Brand(String registrationNum, String name, String context, BrandStatus status, User user, List<CouponBrandRelation> couponBrandRelations, Integer couponCount, Boolean isCouponLimited) {
         this.registrationNum = registrationNum;
         this.name = name;
         this.context = context;
         this.status = status;
         this.user = user;
+        this.couponBrandRelations = couponBrandRelations;
+        this.couponCount = couponCount;
+        this.isCouponLimited = isCouponLimited;
     }
 
     public void refuseBrand(BrandStatus status, String context) {
         this.status = status;
         this.context = context;
     }
+
     public void apply(BrandStatus status) {
         this.status = APPLY;
     }
@@ -67,4 +80,15 @@ public class Brand  extends BaseTimeEntity {
         this.context = context;
         this.status = status;
     }
+
+    // Brand.java
+    public void decreaseCouponCount() {
+        if (Boolean.TRUE.equals(this.isCouponLimited)) {
+            if (this.couponCount == null || this.couponCount <= 0) {
+                throw new IllegalStateException("발급 가능한 쿠폰이 부족합니다.");
+            }
+            this.couponCount--;
+        }
+    }
 }
+
