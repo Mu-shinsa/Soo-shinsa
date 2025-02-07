156af21d331d037285ebcc86672253705fe3638d
FANG <107174112+Fangsangik@users.noreply.github.com>
2025-02-07 22:46:30 +0900

    refactor : "일부 코드 수정"

--- src/main/java/com/Soo_Shinsa/config/RedissonConfig.java
+++ src/main/java/com/Soo_Shinsa/config/RedissonConfig.java
@@ -3,16 +3,23 @@ package com.Soo_Shinsa.config;
 import org.redisson.Redisson;
 import org.redisson.api.RedissonClient;
 import org.redisson.config.Config;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class RedissonConfig {
 
+    @Value("${spring.data.redis.host}")
+    private String redisHost;
+
+    @Value("${spring.data.redis.port}")
+    private int redisPort;
+
     @Bean
     public RedissonClient redissonClient() {
         Config config = new Config();
-        config.useSingleServer().setAddress("redis://localhost:6379")
+        config.useSingleServer().setAddress("redis://" + redisHost + ":" + redisPort)
                 .setConnectionMinimumIdleSize(10)
                 .setConnectionPoolSize(64);
 
